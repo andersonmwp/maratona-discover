75843fb8893917fc39f2f00cd867dfe46432fde9
Anderson <andersonmwp@gmail.com>
2021-02-04 19:26:57 -0300

    :truck: alterando as pastas

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -79,7 +79,7 @@ const DOM = {
             <td class="description">${transaction.description}</td>
             <td class="${CSSclass}">${amount}</td>
             <td class="date">${transaction.date}</td>
-            <td><img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover Transaçãos"></td>
+            <td><img onclick="Transaction.remove(${index})" src="./assets/img/minus.svg" alt="Remover Transaçãos"></td>
         `
         return html
     },
